6ea45aad119d62405363ab21fac0ccf7edf76bef
askot19 <paulohm.ferreira91@gmail.com>
2026-05-12 06:18:05 -0300

    Criação bd, conexão bd

---
+++ Projeto Univesp/config.php
@@ -0,0 +1,20 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName ='formulario_viva_cafe';
+
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword, $dbName)
+
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso";
+    }
+
+?>

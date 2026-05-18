f9355022f3fd6bb5bbdaed24ebda0fe328040ec1
askot19 <paulohm.ferreira91@gmail.com>
2026-05-18 05:16:46 -0300

    Criação de home page para acesso do cliente.

---
+++ Projeto Univesp/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SITE | Restaurante Viva</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+            text-align: center;
+            color: white;
+        }
+        .box{
+            position: absolute;
+        }
+    </style>    
+</head>
+<body>
+    <div class="box">
+        <a href="">Login</a>
+        <a href="">Cadastre-se</a>
+    </div>
+</body>
+</html>
